f60e4ff9520a7aee6986f9e6d2a30356548fbf45
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-04-11 10:16:16 +0200

    ősosztály

---
+++ ForroItalok/ForroItal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForroItalok
+{
+	internal class ForroItal
+	{
+		string nev;
+		int cukor;
+		int mennyiseg;
+
+		public ForroItal(string nev, int cukor, int mennyiseg)
+		{
+			this.nev = nev;
+			this.cukor = cukor;
+			this.mennyiseg = mennyiseg;
+		}
+
+		public override string ToString()
+		{
+			return $"{this.nev} {this.cukor} cukorral {this.mennyiseg} ml";
+		}
+	}
+}
